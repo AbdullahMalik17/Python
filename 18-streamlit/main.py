e2c9572c73431e1280b71bbcec402db8bdd88b7b
Abdullah Ather <abdullahather272@gmail.com>
2025-03-05 20:57:03 +0500

    Updated Data

--- 18-streamlit/main.py
+++ 18-streamlit/main.py
@@ -7,7 +7,6 @@ page = st.sidebar.radio('goto',['Home','Contact Us','About Us'])
 if page == 'Home':
     st.write('Welcome to the Home page')
     st.write('We Built very interactive clothes')
-    st.image('https://images.unsplash.com/photo-1606780840004-4b0b7e3f3b3b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60')
 elif page == 'Contact Us':
     st.text_input('Enter your Name')
     st.text_input('Enter your Email')    
